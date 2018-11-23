d9c8d605445278f5ade3910de4120800ecc276f9
Na Lu <na.lu@btcc.com>
2018-11-23 17:23:17 +0800

    commit

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  publicPath: 'https://static.mobimecdn.com/game/'
+  publicPath: ''
 }
--- config/staging.env.js
+++ config/staging.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"staging"',
-  publicPath: 'https://staging-w.mobiapp.cn/game/'
+  publicPath: ''
 }
